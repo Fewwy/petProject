c958c4f75ef1999bdb6b1dd0529d7a83980a804b
unknown <batteryxv88@yandex.ru>
2022-08-11 21:55:08 +0300

    feat: add slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,13 +2,46 @@ import "./Slider.css";
 import { useEffect, useRef, useState } from "react";
 import coctails from "../Coctails/Coctails";
 import Seasons from "../Seasons/Seasons";
-import SliderOfSeason from "./SliderOfSeason";
+import SliderItems from "./SliderItems";
 
 const Slider = () => {
+  const [date, setDate] = useState();
   const slider = useRef(null);
   const [items, setItems] = useState(coctails);
-
   let position = 0;
+
+  useEffect(() => {
+    fetch("http://worldtimeapi.org/api/timezone/Europe/Moscow")
+      .then((res) => {
+        return res.json();
+      })
+      .then((date) => setDate(date.datetime), setItems(summerArr))
+      .catch((err) => {
+        console.log("Ошибка. Запрос не выполнен: ", err);
+      });
+  }, []);
+
+  let currentDate = new Date(date);
+  const month = currentDate.getMonth() + 1;
+
+  const summerArr = items.filter((item) => item.season === "summer");
+  const GetSummerCoctails = () => {
+    return (
+      summerArr.map(item => <SliderItems img={item.src} />)
+    )
+  }
+  const GetCoctailsComponents = () => {
+    return (
+      items.map(item => <SliderItems img={item.src} />)
+    )
+  }
+  const CurrentCoctails = () => {
+    if (month === 6 || month === 7 || month === 8) {
+      return <GetSummerCoctails />
+    } else {
+      return <GetCoctailsComponents />
+    }
+  }
 
   const prevHandler = () => {
     if (position === 0) {
@@ -38,38 +71,10 @@ const Slider = () => {
     }
   };
 
-  const GetItemOfSeason = () => {
-    fetch("http://worldtimeapi.org/api/timezone/Europe/Moscow")
-      .then((res) => {
-        return res.json();
-      })
-
-      .then((data) => {
-        let date = new Date(data.datetime);
-        const month = date.getMonth() + 1;
-
-        if (month === 6 || month === 7 || month === 8) {
-          const summerArr = items.filter((item) => item.season === "summer");
-
-          summerArr.map((item) => {
-            return (
-              <div
-                className="slider__items"
-                style={{ backgroundImage: `url(${item.src})` }}
-              ></div>
-            );
-          });
-        }
-      })
-      .catch((err) => {
-        console.log("Ошибка. Запрос не выполнен: ", err);
-      });
-  };
-
   return (
     <div className="slider">
       <div className="slider__window" ref={slider}>
-        <SliderOfSeason />
+        <CurrentCoctails />
       </div>
       <button
         className="slider__button slider__button_type_prev"
---
+++ src/components/Slider/SliderItems.js
@@ -0,0 +1,10 @@
+import './Slider.css';
+
+const SliderItems = (props) => {
+  return (  
+    <div className = "slider__items" style={{backgroundImage: `url(${props.img})`}}>
+    </div>
+  );
+}
+ 
+export default SliderItems;
